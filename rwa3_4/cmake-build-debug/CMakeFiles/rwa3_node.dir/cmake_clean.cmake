dbd0fa18594ccd019a15c26ad8f10de345a8ac74
varunasthana92 <varunasthana92@gmail.com>
2020-10-18 21:20:49 -0400

    adding rwa3 files from starter code

---
+++ rwa3_4/cmake-build-debug/CMakeFiles/rwa3_node.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rwa3_node.dir/src/competition.cpp.o"
+  "CMakeFiles/rwa3_node.dir/src/gantry_control.cpp.o"
+  "CMakeFiles/rwa3_node.dir/src/rwa3_node.cpp.o"
+  "CMakeFiles/rwa3_node.dir/src/utils.cpp.o"
+  "devel/lib/rwa3_kootbally/rwa3_node"
+  "devel/lib/rwa3_kootbally/rwa3_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rwa3_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
