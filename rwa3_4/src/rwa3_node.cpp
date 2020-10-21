ab3d72c378ce229ed272c56baea57d1468f9c516
saumilshah66 <shah.saumil66@gmail.com>
2020-10-20 22:11:08 -0400

    Old classes removed and added in different files

--- rwa3_4/src/rwa3_node.cpp
+++ rwa3_4/src/rwa3_node.cpp
@@ -33,140 +33,13 @@
 #include <geometry_msgs/Pose.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h> //--needed for tf2::Matrix3x3
 
+#include "orderBuild.h"
 #include "competition.h"
 #include "utils.h"
 #include "gantry_control.h"
 
 #include <tf2/LinearMath/Quaternion.h>
 
-void orderCallback(const nist_gear::Order& ordermsg) {
-    Order order_recieved;
-    Product product_recieved;
-    Shipment shipment_recieved;
-    order_recieved.order_id = ordermsg.order_id;
-    for(const auto &ship: ordermsg.shipments) {
-        shipment_recieved.shipment_type = ship.shipment_type;
-        shipment_recieved.agv_id = ship.agv_id;
-        for(const auto &prod: ship.products) {
-            product_recieved.type = prod.type;
-            product_recieved.pose = prod.pose;
-            shipment_recieved.products.emplace_back(product_recieved);
-        }
-        order_recieved.shipments.push_back(shipment_recieved);
-    }
-    ROS_INFO_STREAM("I heard: " << order_recieved.order_id);
-    for(auto s: order_recieved.shipments) {
-        ROS_INFO_STREAM("Order type: " << s.shipment_type);
-    }
-}
-
-
-struct similarParts{
-    Part* parts_data;
-    struct similarParts* next;
-};
-
-class allStaticParts{
-private:
-    std::unordered_map<std::string, similarParts* > map;
-public:
-    int getPart(Product prod){
-        std::string name = prod.type;
-        if(map.find(name) != map.end()){
-            similarParts* temp = map[name];
-            if(temp != NULL){
-                map[name] = temp->next;
-            }
-            Part* data = temp->parts_data;
-            delete(temp);
-            prod.p = *data;
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-
-    void setPart(similarParts* data){
-        std::string name = data->parts_data->type;
-        if(map.find(name) != map.end()){
-            data->next = map[name];
-        }
-        map[name] = data;
-        return;
-    }
-};
-
-
-class BuildClass{
-private:
-    std::string pick;
-    allStaticParts non_moving_part_data;
-    bool callBackOnce[16];   // for 16 logical cameras, not including onveyor belt camera cam_id = 1
-
-public:
-
-    BuildClass(){
-        for(int i = 0; i < 16; ++i){
-            callBackOnce[i] = true;
-        }
-    }
-
-    int queryPart(Product prod){
-        return non_moving_part_data.getPart(prod);
-    }
-    void logical_camera_callback(const nist_gear::LogicalCameraImage::ConstPtr & msg, int cam_id){
-        std::cout <<" logial cam id: " << cam_id << "\n";
-        if(cam_id == 1)
-            return;
-        
-        if( callBackOnce[cam_id-2]){
-            ros::Duration timeout(5.0);
-            tf2_ros::Buffer tfBuffer;
-            tf2_ros::TransformListener tfListener(tfBuffer);
-            int i=1, part_idx=1;
-
-            while (i < msg->models.size()){
-                std::string partName = msg->models[i].type;
-                if (i!=1 && msg->models[i].type != msg->models[i-1].type) {
-                    part_idx=1;
-                }
-
-                Part* detected_part = new(Part);
-                detected_part->type = partName;
-                detected_part->pose = msg->models[i].pose;
-                detected_part->id = std::to_string(part_idx);
-                detected_part->state = FREE;        
-
-                std::string frame_name = "logical_camera_" + std::to_string(cam_id) + "_" + msg->models[i].type + "_" + std::to_string(part_idx) + "_frame";
-                
-                detected_part->frame = "logical_camera_" + std::to_string(cam_id);
-                i++;
-                part_idx++;
-                geometry_msgs::TransformStamped transformStamped;
-                transformStamped = tfBuffer.lookupTransform("world", frame_name, ros::Time(0), timeout);
-                // tf2::Quaternion q(  transformStamped.transform.rotation.x,
-                //                     transformStamped.transform.rotation.y,
-                //                     transformStamped.transform.rotation.z,
-                //                     transformStamped.transform.rotation.w);
-                // tf2::Matrix3x3 m(q);
-                // double roll, pitch, yaw;
-                // m.getRPY(roll, pitch, yaw);
-                detected_part->time_stamp = ros::Time(0);
-                detected_part->save_pose.position.x = transformStamped.transform.translation.x;
-                detected_part->save_pose.position.y = transformStamped.transform.translation.y;
-                detected_part->save_pose.position.z = transformStamped.transform.translation.z;
-                detected_part->save_pose.orientation = transformStamped.transform.rotation;
-
-                similarParts* data = new(similarParts);
-                data->parts_data = detected_part;
-                data->next = NULL;
-                non_moving_part_data.setPart(data);
-            }
-            callBackOnce[cam_id - 2] = false;
-        }
-        return;
-    }
-};
 
 
 int main(int argc, char ** argv) {
@@ -211,7 +84,7 @@ int main(int argc, char ** argv) {
     std::string c_state = comp.getCompetitionState();
     comp.getClock();
 
-    ros::Subscriber order_sub = node.subscribe("/ariac/orders", 1000, orderCallback);
+    ros::Subscriber order_sub = node.subscribe("/ariac/orders", 1000, &BuildClass::orderCallback, &buildObj);
 
 
     GantryControl gantry(node);
